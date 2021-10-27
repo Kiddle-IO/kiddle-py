2fb28151503bceed552226e70492c9519369c092
kiddle-io <kiddle-io@users.noreply.github.com>
2021-10-27 23:10:08 +0000

    Added disabmiguation listing and error capture.

--- flaskapp.py
+++ flaskapp.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt
 import wikipedia
 
 from flask import Response
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+
 
 import io
 
@@ -17,22 +16,57 @@ import io
 app = Flask(__name__)
 
 
+#Returns wiki page either as a string or a disambiguation list
+def wikitxt(string):
+    try:
+        bigstring = wikipedia.page(string).content
+        return bigstring
+    except Exception as e: return str(e).split("\n")[1:-2]
+        
+#Generates cloud image
 def wikigen(string, bg='white', cmap='viridis'):
-    bigstring = wikipedia.page(string).content
-    stopwords_session = STOPWORDS
-    stopwords_session.update(['U','S','K'])
-    plt.figure(figsize=(12,12))
-    wordcloud = WordCloud(stopwords = stopwords_session,
-                          background_color=bg,
-                          colormap = cmap,
-                          collocations=False,
-                          width =1200,
-                          height =1000
-                         ).generate(bigstring)
-    plt.axis('off')
-    return wordcloud
-    
-    
+        bigstring = wikipedia.page(string).content
+        stopwords_session = STOPWORDS
+        stopwords_session.update(['b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+        plt.figure(figsize=(12,12))
+        wordcloud = WordCloud(stopwords = stopwords_session,
+                              background_color=bg,
+                              colormap = cmap,
+                              collocations=False,
+                              width =1200,
+                              height =1000
+                             ).generate(bigstring)
+        plt.axis('off')
+        return wordcloud
+
+
+@app.route("/wordcloud", methods=['GET', 'POST'])
+def wordcloud():
+    if request.method == "POST":
+        title = request.form['title']
+        try:
+            newtitle = request.form['ambig']
+        except Exception:
+            newtitle = False
+        print(newtitle)
+        bg = request.form['bg']
+        cmap = request.form['cmap']
+        if isinstance(wikitxt(title), list):
+          txt = wikitxt(title)
+        if newtitle:
+            return render_template('wordcloud.html', title=newtitle, bg=bg, cmap=cmap, wikivalid=True)
+        elif isinstance(wikitxt(title), str):
+            return render_template('wordcloud.html', title=title, bg=bg, cmap=cmap, wikivalid=True)
+        elif isinstance(wikitxt(title), list):
+            if wikitxt(title) == []:
+              return render_template('wordcloud.html', title=title, bg=bg, cmap=cmap, error=True)
+            else:
+              return render_template('wordcloud.html', title=title, bg=bg, cmap=cmap, ambig=txt)               
+    else:
+        return render_template('wordcloud.html')
+
+
+
 @app.route('/generatecloud/<name>/<bg>/<cmap>.png', methods=['GET', 'POST'])
 def plot_png(name, bg, cmap):
     fig = wikigen(name, bg, cmap)
@@ -42,10 +76,6 @@ def plot_png(name, bg, cmap):
     byte_im = buf.getvalue()
     return Response(byte_im, mimetype='image/png')
 
-   
-
-
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -54,17 +84,6 @@ def home():
 @app.route("/projects")
 def about():
     return render_template('projects.html', title='Projects')
-
-@app.route("/wordcloud", methods=['GET', 'POST'])
-def wordcloud():
-    if request.method == "POST":
-        title = request.form['title']
-        bg = request.form['bg']
-        cmap = request.form['cmap']
-        return render_template('wordcloud.html', title=title, bg=bg, cmap=cmap)
-        
-    else:
-        return render_template('wordcloud.html')
 
 
 if __name__ == '__main__':
